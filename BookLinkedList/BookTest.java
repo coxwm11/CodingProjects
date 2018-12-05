d93dbd139dc57afa97cf4bc0d8e529d650eb733c
coxwm11 <coxwm11@gmail.com>
2018-12-04 20:55:30 -0600

    Code prints list

--- BookLinkedList/BookTest.java
+++ BookLinkedList/BookTest.java
@@ -2,92 +2,121 @@ package book;
 
 import java.util.InputMismatchException;
 import java.util.Scanner;
+import java.util.LinkedList;
 
 public class BookTest {
 
-	public static void integersOnly(int num)
-	{
+	public static int integersOnly() {
 		Scanner scan = new Scanner(System.in);
-	
-		
+
 		try {
-			
+
 			// User enters page Number
-			
-			num = scan.nextInt();
-			
+
+			return scan.nextInt();
+
 		}
-		
-		catch (InputMismatchException e){
+
+		catch (InputMismatchException e) {
 			System.out.println("ATTN: Please enter only positive integers.");
 			System.out.println("");
-			
-			
+
+			return -1;
 		}
-		
-		
+
 	}
-	
-	
-	
+
+	public static double doublesOnly() {
+		Scanner scan = new Scanner(System.in);
+
+		try {
+
+			// User enters page Number
+
+			return scan.nextDouble();
+
+		}
+
+		catch (InputMismatchException e) {
+			System.out.println("ATTN: Please enter only positive numbers.");
+			System.out.println("");
+
+			return -1;
+		}
+
+	}
 
 	public static void main(String[] args) {
+
+		LinkedList<Book> myList = new LinkedList<Book>();
 
 		// Introduction
 		System.out.println("\tPrayer Book Store");
 		System.out.println("----------------------------------------");
 
 		Scanner scan = new Scanner(System.in);
-		int serialNumber ;
+		int serialNumber;
 		int pageNumber = 0;
-		int copies =0;
+		int copies = 0;
 		double bookCost = 0;
 		String bookTitle = null;
 		String authorName = null;
 
-		// User enters ISBN
-		System.out.print("Enter an ISBN or 000 to stop: ");
-		serialNumber = scan.nextInt();
-
-		while (serialNumber != 000) {
-			
-
-			// User enters title
-			System.out.print("Enter book title: ");
-
-			// flush the Buffer
-			String flush = scan.nextLine();
-			bookTitle = scan.nextLine();
-
-			// User enters author
-			System.out.print("Enter author: ");
-			authorName = scan.nextLine();
-
-			
-			System.out.print("Enter number of pages in the book: ");
-			integersOnly(pageNumber);
-
-			// User enters price of Book
-			System.out.print("Enter the price of the book: ");
-			bookCost = scan.nextInt();
-
-			// User enters Number of copies in stock
-			System.out.print("Number of Copies in Stock: ");
-			copies = scan.nextInt();
-
-			System.out.println("");
+		do {
 
 			// User enters ISBN
-			System.out.println("Enter an ISBN or 000 to stop: ");
+			System.out.print("Enter an ISBN or 000 to stop: ");
 			serialNumber = scan.nextInt();
 
-		}
+			if (serialNumber != 000)
 
-		//Book takes input from user for isbn, title, author, price, copies available
-		
-		Book b = new Book(serialNumber, pageNumber,copies, bookTitle, authorName,bookCost);
-		
-		System.out.println(b.toString());
+			{
+				// User enters title
+				System.out.print("Enter book title: ");
+
+				// flush the Buffer
+				String flush = scan.nextLine();
+				bookTitle = scan.nextLine();
+
+				// User enters author
+				System.out.print("Enter author: ");
+				authorName = scan.nextLine();
+
+				// User enters page Number
+				do {
+					System.out.print("Enter number of pages in the book: ");
+					pageNumber = integersOnly();
+
+				} while (pageNumber == -1);
+
+				do {
+					// User enters price of Book
+					System.out.print("Enter the price of the book: ");
+					bookCost = doublesOnly();
+				} while (bookCost == -1);
+
+				do {
+					// User enters Number of copies in stock
+					System.out.print("Number of Copies in Stock: ");
+					copies = integersOnly();
+
+				} while (copies == -1);
+
+			}
+			System.out.println("");
+
+			Book b = new Book(serialNumber, pageNumber, copies, bookTitle, authorName, bookCost);
+
+			myList.add(b);
+
+			System.out.println(b.toString());
+
+		} while (serialNumber != 000);
+
+		for (Book b : myList) {
+			System.out.println(b);
+		}
+		// Book takes input from user for isbn, title, author, price, copies available
 
 	}
 
